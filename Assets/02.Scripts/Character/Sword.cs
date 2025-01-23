5e5d49ec002e23b98c52e8ea7c0d9ffe0e711d78
Seobok <Seobok@users.noreply.github.com>
2025-01-23 16:15:39 +0900

    점수가 정상적으로 출력되지 않던 문제 해결
    - 정상적인 KillCount를 받아오지 못해 정상적으로 출력되지 않던 문제 해결
    - 직관적으로 네이밍 수정

--- Assets/02.Scripts/Character/Sword.cs
+++ Assets/02.Scripts/Character/Sword.cs
@@ -8,30 +8,31 @@ namespace HideAndSkull.Character
     [RequireComponent(typeof(BoxCollider))]
     public class Sword : MonoBehaviour
     {
-        public Skull Owner { get; set; }
+        public Skull SwordOwner { get; set; }
         private void OnTriggerEnter(Collider other)
         {
-            //맞았는지 판단은 MasterClient에서만 함
+            //맞았는지 판단은 맞은 Skull에서 함
             if(other.TryGetComponent(out PhotonView photonView) && 
                photonView.IsMine)
             {
-                if (other.TryGetComponent(out Skull skull))
+                if (other.TryGetComponent(out Skull attackedSkull))
                 {
-                    if (skull.isDead)
+                    if (attackedSkull.isDead)
                         return;
 
-                    //MasterClient에서 맞았다고 판단되면 모든 플레이어에게 해당 character가 죽었다고 호출함
-                    skull.PhotonView.RPC(nameof(skull.Die), RpcTarget.All);
-                    skull.PlayerCustomProperty["IsDead"] = true;
-                    photonView.Owner.SetCustomProperties(skull.PlayerCustomProperty);
-                    UI_ToastPanel uI_ToastPanel = UI_Manager.instance.Resolve<UI_ToastPanel>();
-                    uI_ToastPanel.ShowToast($"{photonView.Owner.NickName}님이 사망하였습니다.");
+                    //모든 플레이어에게 해당 character가 죽었다고 호출함
+                    attackedSkull.PhotonView.RPC(nameof(attackedSkull.Die), RpcTarget.All);
 
-                    if (skull.PlayMode == PlayMode.Player)
+                    if (attackedSkull.PlayMode == PlayMode.Player)
                     {
-                        int killcount = (int)Owner.PlayerCustomProperty["KillCount"];
-                        Owner.PlayerCustomProperty["KillCount"] = killcount + 1;
-                        Owner.PhotonView.Owner.SetCustomProperties(Owner.PlayerCustomProperty);
+                        int killcount = (int)SwordOwner.PhotonView.Owner.CustomProperties["KillCount"];
+                        SwordOwner.PlayerCustomProperty["KillCount"] = killcount + 1;
+                        SwordOwner.PhotonView.Owner.SetCustomProperties(SwordOwner.PlayerCustomProperty);
+
+                        attackedSkull.PlayerCustomProperty["IsDead"] = true;
+                        attackedSkull.PhotonView.Owner.SetCustomProperties(attackedSkull.PlayerCustomProperty);
+                        UI_ToastPanel uI_ToastPanel = UI_Manager.instance.Resolve<UI_ToastPanel>();
+                        uI_ToastPanel.ShowToast($"{photonView.Owner.NickName}님이 사망하였습니다.");
                     }
                 }
             }
