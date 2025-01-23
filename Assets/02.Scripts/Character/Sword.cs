e1a63e2d4e225c72b4cf66ae69f5ffdaa414674b
Seobok <Seobok@users.noreply.github.com>
2025-01-23 15:42:19 +0900

    AI와 MasterClient를 혼동하던 문제 해결
    - AI와 MasterClient를 혼동하던 문제 해결

--- Assets/02.Scripts/Character/Sword.cs
+++ Assets/02.Scripts/Character/Sword.cs
@@ -11,7 +11,7 @@ namespace HideAndSkull.Character
         public Skull Owner { get; set; }
         private void OnTriggerEnter(Collider other)
         {
-            //맞았는지 판단은 MasterClient에서만 함
+            //맞았는지 판단은 맞은 Skull에서 함
             if(other.TryGetComponent(out PhotonView photonView) && 
                photonView.IsMine)
             {
@@ -20,15 +20,15 @@ namespace HideAndSkull.Character
                     if (skull.isDead)
                         return;
 
-                    //MasterClient에서 맞았다고 판단되면 모든 플레이어에게 해당 character가 죽었다고 호출함
+                    //모든 플레이어에게 해당 character가 죽었다고 호출함
                     skull.PhotonView.RPC(nameof(skull.Die), RpcTarget.All);
-                    skull.PlayerCustomProperty["IsDead"] = true;
-                    photonView.Owner.SetCustomProperties(skull.PlayerCustomProperty);
-                    UI_ToastPanel uI_ToastPanel = UI_Manager.instance.Resolve<UI_ToastPanel>();
-                    uI_ToastPanel.ShowToast($"{photonView.Owner.NickName}님이 사망하였습니다.");
 
                     if (skull.PlayMode == PlayMode.Player)
                     {
+                        skull.PlayerCustomProperty["IsDead"] = true;
+                        photonView.Owner.SetCustomProperties(skull.PlayerCustomProperty);
+                        UI_ToastPanel uI_ToastPanel = UI_Manager.instance.Resolve<UI_ToastPanel>();
+                        uI_ToastPanel.ShowToast($"{photonView.Owner.NickName}님이 사망하였습니다.");
                         int killcount = (int)Owner.PlayerCustomProperty["KillCount"];
                         Owner.PlayerCustomProperty["KillCount"] = killcount + 1;
                         Owner.PhotonView.Owner.SetCustomProperties(Owner.PlayerCustomProperty);
